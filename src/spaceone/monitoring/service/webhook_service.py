87a8b2e15285f33db3acfff861bac0883fac5b47
LimChaeyeon <cylim@mz.co.kr>
2025-10-29 20:57:38 +0900

    add log

--- src/spaceone/monitoring/service/webhook_service.py
+++ src/spaceone/monitoring/service/webhook_service.py
@@ -5,23 +5,24 @@ from spaceone.monitoring.error import *
 
 _LOGGER = logging.getLogger(__name__)
 
+
 @authentication_handler
 @authorization_handler
 @event_handler
 class WebhookService(BaseService):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    @check_required(['options'])
+    @check_required(["options"])
     def init(self, params):
-        """ init grpc by options
-                """
-        return {'metadata': {}}
+        """init grpc by options"""
+        _LOGGER.debug(f">>>>>>>>>> [WebhookService: init] params: {params}")
+        return {"metadata": {}}
 
     @transaction
-    @check_required(['options'])
+    @check_required(["options"])
     def verify(self, params):
+        _LOGGER.debug(f">>>>>>>>>> [WebhookService: verify] params: {params}")
         """
         Args:
               params:
